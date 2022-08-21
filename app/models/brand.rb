202f1eafccaa34dd07b3afbe8b7f249cd95332c9
FUJITASHOGO2190 <mr.shouuge@gmail.com>
2022-08-21 18:23:07 +0900

    投稿機能実装

---
+++ app/models/brand.rb
@@ -0,0 +1,18 @@
+class Brand < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '選択する' },
+    { id: 1, name: 'Levis' },
+    { id: 2, name: 'Lee' },
+    { id: 3, name: 'EDWIN' },
+    { id: 4, name: '桃太郎ジーンス' },
+    { id: 5, name: 'JapanBuleJeans' },
+    { id: 6, name: 'FULLCOUNT' },
+    { id: 7, name: 'DENIME' },
+    { id: 8, name: 'WAREHOUSE' },
+    { id: 9, name: 'RRL' },
+    { id: 10, name: 'その他' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :posts
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  with_options presence: true do
+    validates :user_id
+    validates :content
+    validates :washing_frequency
+    validates :wearing_years
+    validates :brand_id
+    validates :image
+  end
+
+  validates :brand_id, numericality: { other_than: 0 }
+
+  belongs_to :user
+  has_one_attached :image
+
+  belongs_to :brand
+
 end
