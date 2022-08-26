64bb9d030c44aaa1639a7b69510025c4864ff9c6
FUJITASHOGO2190 <mr.shouuge@gmail.com>
2022-08-26 14:53:06 +0900

    マイページ実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy, :update]
 
   def index
-    @posts = Post.all.order('created_at DESC')
+    @posts = Post.includes(:user).order('created_at DESC')
   end
 
   def new
