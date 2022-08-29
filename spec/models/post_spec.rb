44973df8f6d885a90b6fca4caec40c3b93df8776
FUJITASHOGO2190 <mr.shouuge@gmail.com>
2022-08-29 14:43:17 +0900

    テストコード修正

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -17,41 +17,36 @@ RSpec.describe Post, type: :model do
       end
     end 
 
-      context '出品ができないとき' do
-        it 'ユーザー登録している人でないと出品できない' do
-          @post.user_id = nil
-          @post.valid?
-          expect(@post.errors.full_messages).to include("User can't be blank")
-        end
-        it '１枚画像がないと出品できない' do
+      context '投稿ができないとき' do
+        it '１枚画像がないと投稿できない' do
           @post.images = nil
           @post.valid?
-          expect(@post.errors.full_messages).to include("Images can't be blank")
+          expect(@post.errors.full_messages).to include("写真を選択して下さい")
         end
         it '文章が空欄だと投稿できない' do
           @post.content = nil
           @post.valid?
-          expect(@post.errors.full_messages).to include("Content can't be blank")
+          expect(@post.errors.full_messages).to include("コメントを入力してください")
         end
         it '洗濯頻度が空欄だと投稿できない' do
           @post.washing_frequency = nil
           @post.valid?
-          expect(@post.errors.full_messages).to include("Washing frequency can't be blank")
+          expect(@post.errors.full_messages).to include("洗濯頻度を入力してください")
         end
         it '着用年数が空欄だと投稿できない' do
           @post.wearing_years = nil
           @post.valid?
-          expect(@post.errors.full_messages).to include("Wearing years can't be blank")
+          expect(@post.errors.full_messages).to include("着用年数を入力してください")
         end
         it 'ブランドの情報が空欄だと投稿できない' do
           @post.brand_id = nil
           @post.valid?
-          expect(@post.errors.full_messages).to include("Brand can't be blank")
+          expect(@post.errors.full_messages).to include("ブランドを入力してください")
         end
         it 'ブランドの情報が「---」だと投稿できない' do
           @post.brand_id = 0
           @post.valid?
-          expect(@post.errors.full_messages).to include("Brand must be other than 0")
+          expect(@post.errors.full_messages).to include("ブランドを選択して下さい")
         end
       end  
   end  
