60831d097b3eee50cf5eba160c7954e72e213c39
FUJITASHOGO2190 <mr.shouuge@gmail.com>
2022-09-04 17:47:02 +0900

    コメント削除機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,18 @@
 class CommentsController < ApplicationController
 
+  before_action :authenticate_user!, only: [:create, :destroy]
+
   def create
     @comment = Comment.new(comment_params)
     @comment.save
     redirect_to post_path(@comment.post)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @post_comments = @post.comments
+    Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    redirect_to post_path(@post.id)
   end
 
   private
